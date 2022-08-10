5dab5a8e705a38334d92de8a6ef4d58550b121c8
Vansitha <vansitha1@gmail.com>
2022-08-10 12:03:47 +0530

    Complete build without contact.

--- js/script.js
+++ js/script.js
@@ -12,4 +12,4 @@ var typed = new Typed(".auto-type", options);
 // add alret when resume button is clicked
 
 var resumeButton = document.querySelector(".main-button");
-resumeButton.addEventListener("click", function() { alert("Coming soon!") });
+resumeButton.addEventListener("click", function() { alert("Will be available soon!") });
