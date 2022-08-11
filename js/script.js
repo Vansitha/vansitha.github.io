477c65c13f6049d7014ddeb650dd3b6e5a650b88
Vansitha <vansitha1@gmail.com>
2022-08-11 21:03:55 +0530

    Update footer text

--- js/script.js
+++ js/script.js
@@ -9,16 +9,7 @@ var options = {
 
 var typed = new Typed(".auto-type", options);
 
-var option2 = {
-    strings: ["Designing layout.", "Coding it up.", "Squashing bugs."],
-    typeSpeed: 100,
-    backSpeed: 100,
-    loop: true,
-}
-
-var comingSoonType = new Typed(".auto-type-coming-soon", option2);
-
 // add alret when resume button is clicked
 
 var resumeButton = document.querySelector(".main-button");
-resumeButton.addEventListener("click", function() { alert("Available soon!") });
+resumeButton.addEventListener("click", () => { alert("Available soon!"); });
