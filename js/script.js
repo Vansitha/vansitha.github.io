2af31cc4ea6aa4bfc036bd5f0c7b199e0772ccf0
Vansitha <vansitha1@gmail.com>
2022-08-13 21:22:05 +0530

    Update content

--- js/script.js
+++ js/script.js
@@ -12,4 +12,4 @@ var typed = new Typed(".auto-type", options);
 // add alret when resume button is clicked
 
 var resumeButton = document.querySelector(".main-button");
-resumeButton.addEventListener("click", () => { alert("Opps Vansitha hasn't attached his resume yet. Seems like he is working on it! :)"); });
+resumeButton.addEventListener("click", () => { alert("Opps! Vansitha hasn't attached his resume yet. Seems like he is working on it! :)"); });
