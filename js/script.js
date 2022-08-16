f2a08b5a490170eb139c5a03c974b1b207cd5b97
Vansitha <vansitha1@gmail.com>
2022-08-16 10:37:35 +0530

    Disabled form functionality after test release

--- js/script.js
+++ js/script.js
@@ -51,8 +51,3 @@ aboutSection.addEventListener("click", () => { smoothScroll("#project", 1000); }
 
 var aboutSection = document.querySelector("#contact");
 aboutSection.addEventListener("click", () => { smoothScroll(".contact", 1000); });
-
-// Contact form 
-$('form').on('submit', function(e) {
-    e.preventDefault();
-});
