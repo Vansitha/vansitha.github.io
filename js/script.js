14459284f4ba59ce3ea576be1d0af52d25edc14a
Vansitha <vansitha1@gmail.com>
2022-10-06 20:25:36 +0530

    Update scroll animation

--- js/script.js
+++ js/script.js
@@ -22,10 +22,17 @@ function smoothScroll(target, duration) {
     var distance = targetPosition - startPosition;
     var startTime = null;
 
+var windowSize = document.documentElement.clientWidth || window.innerWidth;
+var minScreenWidth = 400;
+
     function animation(currentTime) {
         if (startTime == null) startTime = currentTime;
         var timeElaplsed = currentTime - startTime;
-        var run = easeOut(timeElaplsed, startPosition, distance, duration);
+        if (windowSize <= minScreenWidth) {
+            var run = mobileAnimation(timeElaplsed, startPosition, distance, duration);
+        } else {
+            var run = easeOut(timeElaplsed, startPosition, distance, duration);
+        }
         window.scrollTo(0, run);
         if (timeElaplsed < duration) requestAnimationFrame(animation);
     }
@@ -40,19 +47,24 @@ function smoothScroll(target, duration) {
         return -c/2 * (t*t*t*t - 2) + b;
 
     };
-    
+
+    function mobileAnimation(t, b, c, d) {
+        return c*t/d + b;
+    }
+
 
     requestAnimationFrame(animation);
 }
 
-var aboutSection = document.querySelector("#about");
-aboutSection.addEventListener("click", () => { smoothScroll("#about-me", 1000); });
 
-var aboutSection = document.querySelector(".next-page-icon");
-aboutSection.addEventListener("click", () => { smoothScroll("#about-me", 1000); });
+    var aboutSection = document.querySelector("#about");
+    aboutSection.addEventListener("click", () => { smoothScroll("#about-me", 1000); });
 
-var aboutSection = document.querySelector("#projects");
-aboutSection.addEventListener("click", () => { smoothScroll("#project", 1000); });
+    var aboutMeSection = document.querySelector(".next-page-icon");
+    aboutMeSection.addEventListener("click", () => { smoothScroll("#about-me", 1000); });
 
-var aboutSection = document.querySelector("#contact");
-aboutSection.addEventListener("click", () => { smoothScroll(".contact", 1000); });
+    var projectsSection = document.querySelector("#projects");
+    projectsSection.addEventListener("click", () => { smoothScroll("#project", 1000); });
+
+    var contactSection = document.querySelector("#contactMe");
+    contactSection.addEventListener("click", () => { smoothScroll("#contact", 1000); });
